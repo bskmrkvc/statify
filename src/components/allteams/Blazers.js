ed1d59f8fcae31fe44f3f1c349aefddc33422898
bskmrkvc <markovic.bosko2000@gmail.com>
2022-10-15 14:36:51 +0200

    Updated Blazers roster

--- src/components/allteams/Blazers.js
+++ src/components/allteams/Blazers.js
@@ -3,23 +3,21 @@ import './BlazersTeamStyle.css';
 
 
 const players = [
-  {id: 1, name: 'Darius Garland', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1629636.png' , jerseyNumber: '10',  position: 'G', country: 'USA'},
-  {id: 2, name: 'John Collins', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1628381.png' , jerseyNumber: '20',  position: 'F-C', country: 'USA'},
-  {id: 3, name: 'Bogdan Bogdanovic', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/203992.png' , jerseyNumber: '13',  position: 'G', country: 'SRB'},
-  {id: 4, name: "De'Andre Hunter", logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1629631.png' , jerseyNumber: '12',  position: 'F-G', country: 'USA'},
-  {id: 5, name: 'Kevin Huerter', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1628989.png' , jerseyNumber: '3',  position: 'G-F', country: 'USA'},
-  {id: 6, name: 'Clint Capela', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/203991.png' , jerseyNumber: '15',  position: 'C', country: 'SUI'},
-  {id: 7, name: 'Danilo Gallinari', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/201568.png' , jerseyNumber: '8',  position: 'F', country: 'ITA'},
-  {id: 8, name: 'Onyeka Okongwu', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1630168.png' , jerseyNumber: '17',  position: 'F-C', country: 'USA'},
-  {id: 9, name: 'Dellon Wright', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1626153.png' , jerseyNumber: '0',  position: 'G', country: 'USA'},
-  {id: 10, name: 'Timothé Luwawu-Cabarrot', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1627789.png' , jerseyNumber: '7',  position: 'G-F', country: 'FRA'},
-  {id: 11, name: 'Lou Williams', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/101150.png' , jerseyNumber: '6',  position: 'G', country: 'USA'},
-  {id: 12, name: 'Chaundee Brown Jr.', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1630602.png' , jerseyNumber: '38',  position: 'F', country: 'USA'},
-  {id: 13, name: 'Gorgui Dieng', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/203476.png' , jerseyNumber: '10',  position: 'C', country: 'SEN'},
-  {id: 14, name: 'Kevin Knox', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1628995.png' , jerseyNumber: '5',  position: 'F', country: 'USA'},
-  {id: 15, name: 'Skylar Mays', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1630219.png' , jerseyNumber: '4',  position: 'G', country: 'USA'},
-  {id: 16, name: 'Jalen Johnson', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1630552.png' , jerseyNumber: '1',  position: 'F', country: 'USA'},
-  {id: 17, name: 'Sharife Cooper', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1630536.png' , jerseyNumber: '2',  position: 'G', country: 'USA'}
+  {id: 1, name: 'Damian Lillard', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/203081.png' , jerseyNumber: '0',  position: 'G', country: 'USA'},
+  {id: 2, name: 'Jerami Grant', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/203924.png' , jerseyNumber: '9',  position: 'F', country: 'USA'},
+  {id: 3, name: 'Josh Hart', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1628404.png' , jerseyNumber: '11',  position: 'G', country: 'USA'},
+  {id: 4, name: 'Anfernee Simmons', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1629014.png' , jerseyNumber: '1',  position: 'G', country: 'USA'},
+  {id: 5, name: 'Jusuf Nurkić', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/203994.png' , jerseyNumber: '27',  position: 'C', country: 'BIH'},
+  {id: 6, name: 'Nassir Little', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1629642.png' , jerseyNumber: '10',  position: 'F-G', country: 'USA'},
+  {id: 7, name: 'Keon Johnson', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1630553.png' , jerseyNumber: '6',  position: 'G', country: 'USA'},
+  {id: 8, name: 'Trendon Watford', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1630570.png' , jerseyNumber: '2',  position: 'F', country: 'USA'},
+  {id: 9, name: 'Gary Payton II', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1627780.png' , jerseyNumber: '00',  position: 'G', country: 'USA'},
+  {id: 10, name: 'Justise Winslow', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1626159.png' , jerseyNumber: '26',  position: 'F-G', country: 'FRA'},
+  {id: 11, name: 'Greg Brown III', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1630535.png' , jerseyNumber: '4',  position: 'F', country: 'USA'},
+  {id: 12, name: 'Shaedon Sharpe', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1631101.png' , jerseyNumber: '17',  position: 'G', country: 'USA'},
+  {id: 13, name: 'Drew Eubanks', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1629234.png' , jerseyNumber: '24',  position: 'F-C', country: 'USA'},
+  {id: 14, name: 'Olivier Sarr', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1630846.png' , jerseyNumber: '30',  position: 'C', country: 'FRA'},
+  {id: 15, name: 'Jabari Walker', logoURL: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1631133.png' , jerseyNumber: '34',  position: 'F', country: 'USA'}
 ];
 
 
@@ -115,8 +113,6 @@ function Blazers() {
                 <Player players={players[12]}/>
                 <Player players={players[13]}/>
                 <Player players={players[14]}/>
-                <Player players={players[15]}/>
-                <Player players={players[16]}/>
               </ul>
             </div>
           </div>
